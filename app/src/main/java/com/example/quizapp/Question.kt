3a3bf3a571c8f4db3a273984529479d26fed7b12
sonms <71806591+sonms@users.noreply.github.com>
2022-09-05 21:47:23 +0900

    Update Question.kt

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -1,9 +1,9 @@
 package com.example.quizapp
 
 data class Question(
-    val id: Int,
-    val question: String,
-    val image: Int,
+    val id: Int, //문제 아이디값(몇 번째 문제인지)
+    val question: String,//문제
+    val image: Int,//문제에 보여질 이미지
     val optionOne: String, //option은 객관식 답변들
     val optionTwo: String,
     val optionThree: String,
